1c46668c083548a00049f7448341dbd159a2e3dc
jiaqi.cai <caijiaqi11633@163.com>
2018-04-11 23:01:38 +0800

    add articles html

--- beeblog/routers/router.go
+++ beeblog/routers/router.go
@@ -13,4 +13,7 @@ func init() {
 
 	beego.Router("/login", &controllers.LoginController{})
 	beego.Router("/category", &controllers.CategoryController{})
+	beego.Router("/topic", &controllers.TopicController{})
+
+	beego.AutoRouter(&controllers.TopicController{})
 }
